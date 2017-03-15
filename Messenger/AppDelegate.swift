96438dadaea928cd81da456ddc3c24bd2ff4e8ee
Daniel Gormly <dgormly@me.com>
2017-03-15 15:58:50 +1000

    Initial Commit

---
+++ Messenger/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Messenger
+//
+//  Created by Daniel Gormly on 15/3/17.
+//  Copyright © 2017 Daniel Gormly. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Messenger/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Daniel Gormly on 15/3/17.
+//  Copyright © 2017 Daniel Gormly. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
